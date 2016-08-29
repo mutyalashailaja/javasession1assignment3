e7e1fd5b188a39deae5ee8286e566dec85b79444
mutyalashailaja <mutyala.shailaja@gmail.com>
2016-08-29 16:57:29 +0530

    session1assignment4

---
+++ src/datatype/datatypeusage.java
@@ -0,0 +1,14 @@
+package datatype;
+
+public class datatypeusage {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a="adithi";
+		int b=10.5;
+		int c=5;
+		
+
+	}
+
+}
